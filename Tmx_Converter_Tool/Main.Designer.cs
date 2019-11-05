5a94ec4587261a772b867d589ea855ca32d7f3af
Nhut Vinh <nhutvinh14@gmail.com>
2019-11-05 14:10:00 +0700

    Add project files.

---
+++ Tmx_Converter_Tool/Main.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Tmx_Converter_Tool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblMapTexture = new System.Windows.Forms.Label();
+            this.lblTileWidth = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblTileHeight = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lsvObject = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btnImport.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImport.Location = new System.Drawing.Point(352, 447);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(219, 54);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "( . )( . )";
+            this.btnImport.UseVisualStyleBackColor = false;
+            this.btnImport.Click += new System.EventHandler(this.BtnImport_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(127, 23);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(42, 20);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "XXX";
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "fileDialog";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Patch :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Map Width :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Map Height :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tile Width :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(301, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tile Height :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Texture ID MAP :";
+            // 
+            // lblMapTexture
+            // 
+            this.lblMapTexture.AutoSize = true;
+            this.lblMapTexture.Location = new System.Drawing.Point(201, 67);
+            this.lblMapTexture.Name = "lblMapTexture";
+            this.lblMapTexture.Size = new System.Drawing.Size(46, 20);
+            this.lblMapTexture.TabIndex = 8;
+            this.lblMapTexture.Text = " XXX";
+            // 
+            // lblTileWidth
+            // 
+            this.lblTileWidth.AutoSize = true;
+            this.lblTileWidth.Location = new System.Drawing.Point(146, 151);
+            this.lblTileWidth.Name = "lblTileWidth";
+            this.lblTileWidth.Size = new System.Drawing.Size(46, 20);
+            this.lblTileWidth.TabIndex = 9;
+            this.lblTileWidth.Text = " XXX";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(146, 109);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(46, 20);
+            this.lblWidth.TabIndex = 10;
+            this.lblWidth.Text = " XXX";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(419, 109);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(46, 20);
+            this.lblHeight.TabIndex = 11;
+            this.lblHeight.Text = " XXX";
+            // 
+            // lblTileHeight
+            // 
+            this.lblTileHeight.AutoSize = true;
+            this.lblTileHeight.Location = new System.Drawing.Point(419, 151);
+            this.lblTileHeight.Name = "lblTileHeight";
+            this.lblTileHeight.Size = new System.Drawing.Size(46, 20);
+            this.lblTileHeight.TabIndex = 12;
+            this.lblTileHeight.Text = " XXX";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(34, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "List Object :";
+            // 
+            // lsvObject
+            // 
+            this.lsvObject.HideSelection = false;
+            this.lsvObject.Location = new System.Drawing.Point(38, 235);
+            this.lsvObject.Name = "lsvObject";
+            this.lsvObject.Size = new System.Drawing.Size(533, 195);
+            this.lsvObject.TabIndex = 14;
+            this.lsvObject.UseCompatibleStateImageBehavior = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(595, 539);
+            this.Controls.Add(this.lsvObject);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblTileHeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblTileWidth);
+            this.Controls.Add(this.lblMapTexture);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnImport);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(617, 595);
+            this.MinimumSize = new System.Drawing.Size(617, 595);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tmx Converter Tool By maslhiro (\")>";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblMapTexture;
+        private System.Windows.Forms.Label lblTileWidth;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblTileHeight;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListView lsvObject;
+    }
+}
---
+++ Tmx_Converter_Tool/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TiledSharp;
+
+namespace Tmx_Converter_Tool
+{
+    public partial class Main : Form
+    {
+        private string idTexture;
+        private int mapWidth, mapHeight, tileWidth, tileHeight;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //lsvObject.View = View.Details;
+        }
+
+        private void BtnImport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = fileDialog.ShowDialog();
+                if (result == DialogResult.OK) // Test result.
+                {
+                    var path = fileDialog.FileName;
+                    var map = new TmxMap(path);
+
+                    if (path.Length > 52) lblFileName.Text = "\\\\..." + path.Substring(path.Length - 52, 52);
+                    else lblFileName.Text = path;
+
+                    // map chi co 1 tileset duy nhat nen ko duyet het tileset lam gi :)))
+                    idTexture = map.Tilesets[0].Name;
+                    lblMapTexture.Text = idTexture;
+
+                    tileWidth = map.TileWidth;
+                    tileHeight = map.TileHeight;
+
+                    mapWidth = map.Width * tileWidth;
+                    mapHeight = map.Height * tileHeight;
+
+                    lblTileWidth.Text = tileWidth.ToString();
+                    lblTileHeight.Text = tileHeight.ToString();
+
+                    lblWidth.Text = mapWidth.ToString();
+                    lblHeight.Text = mapHeight.ToString();
+
+                    // Duyet cac obj
+                    var listObjectGroups = map.ObjectGroups;
+
+                    foreach (var group in listObjectGroups)
+                    {
+                        foreach (var obj in group.Objects)
+                        {
+                            lsvObject.Items.Add(obj.Name);
+
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), " --=== Chọn cho đúng file vào ===--");
+            }
+
+        }
+    }
+}
